9bad4191beab7c8caec7ad0e5eab65112c6ed36e
Jason Lengstorf <jason@lengstorf.com>
2020-05-26 18:01:01 -0700

    feat: drop deprecated features (fix #2)

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ const glob = require('glob');
 const { injectScript } = require('./inject-script');
 
 module.exports = {
-  // Name of the Netlify Build plugin. Should match the package name on npm.
-  name: 'netlify-plugin-pastel',
-  async onPostBuild({ constants: { BUILD_DIR } }) {
+  async onPostBuild({ constants: { PUBLISH_DIR } }) {
     // bail immediately if this isn’t a deploy preview
     if (process.env.CONTEXT !== 'deploy-preview') return;
 
     // find all HTML files in the build directory
-    const files = glob.sync(`${BUILD_DIR}/**/*.html`);
+    const files = glob.sync(`${PUBLISH_DIR}/**/*.html`);
 
     // run all the file replacements in parallel...
     const promises = files.map(injectScript);
@@ -19,8 +17,8 @@ module.exports = {
     await Promise.all(promises);
 
     // make sure frames are allowed so Pastel can do its thang
-    const [headersFile] = glob.sync(`${BUILD_DIR}/_headers`);
-    
+    const [headersFile] = glob.sync(`${PUBLISH_DIR}/_headers`);
+
     if (headersFile) {
       const headers = fs.readFileSync(headersFile, 'utf8');
       const cleaned = headers.replace(/^\s*x-frame-options:.*$/gim, '');
